31b00b6e08c5d02a6accba589fabd3ae69de9eb6
Aleksander Okonski <aleksander.oko@attackpointsecurity.com>
2021-02-07 16:36:09 +0100

    Fixed sha256 bugs

--- main.go
+++ main.go
@@ -7,15 +7,41 @@ import (
 	"encoding/hex"
 	"fmt"
 	"io/ioutil"
+	gobig "math/big"
 
 	"github.com/minvws/nl-covid19-coronatester-ctcl-core/holder"
 	"github.com/minvws/nl-covid19-coronatester-ctcl-core/issuer"
 	"github.com/minvws/nl-covid19-coronatester-ctcl-core/verifier"
 	"github.com/privacybydesign/gabi"
+	qrcode "github.com/skip2/go-qrcode"
 )
+
+// SHA256(example-fhir-nl.bin)= 67409d726c4213eabe52bd6ac5a8c4624f601c0421541facb6ee49ebb0d867a4
+const fileFhir = "example-fhir-nl.bin"
+
+// SHA256(example-fhir-cz.bin)= f38dc38f61c78d6b80c4b8af18fdf6b78fd5dd68c6f0d4878d21e9f8363f9f0b
+// const fileFhir = "example-fhir-cz.bin"
 
 func main() {
 	showFHIRExample()
+}
+
+var qrCharset = []byte("0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ $%*+-./:")
+var qrCharsetLen = gobig.NewInt(45)
+
+func qrEncode(input []byte) []byte {
+	estOutputLen := int(float64(len(input))*1.4568) + 1
+	output := make([]byte, 0, estOutputLen)
+
+	divident, remainder := new(gobig.Int), new(gobig.Int)
+	divident.SetBytes(input)
+
+	for len(divident.Bits()) != 0 {
+		divident, remainder = divident.QuoRem(divident, qrCharsetLen, remainder)
+		output = append(output, qrCharset[remainder.Int64()])
+	}
+
+	return output
 }
 
 func showFHIRExample() {
@@ -77,7 +103,7 @@ func showFHIRExample() {
 	encryptedMessageL2 := RSA_OAEP_Encrypt(fhirl2, publicKey)
 
 	sha256_ekl2 := sha256.New()
-	sha256_ekl2.Write(encryptedMessageL1)
+	sha256_ekl2.Write(encryptedMessageL2)
 
 	fmt.Printf("    Encrypted FHIR record level 2\n")
 	fmt.Printf("    	Private key: %v\n", privateKey)
@@ -86,7 +112,7 @@ func showFHIRExample() {
 
 	////
 
-	attributeValues := [][]byte{encryptedMessageL1, sha256_ekl1.Sum(nil), encryptedMessageL2, sha256_ekl2.Sum(nil)}
+	attributeValues := [][]byte{encryptedMessageL1, []byte(hex.EncodeToString(sha256_ekl1.Sum(nil))), encryptedMessageL2, []byte(hex.EncodeToString(sha256_ekl2.Sum(nil)))}
 	ism := issuer.Issue(issuerPkXml, issuerSkXml, issuerNonce, attributeValues, icm)
 
 	cred, err := holder.CreateCredential(credBuilder, ism, attributeValues)
@@ -98,8 +124,8 @@ func showFHIRExample() {
 	fmt.Printf("4) Citizen (Holder) gets the issuer its public key (%v) to check the signature.\n", issuerPk.Issuer)
 
 	fmt.Println("5) Citizen (Holder) now goes into the wild")
-	count := 5
-	for i := 0; i < count; i++ {
+
+	for i := 0; i < 5; i++ {
 		fmt.Printf("\n")
 		fmt.Printf("    * An Encounter happens!\n")
 		fmt.Printf("       Citizen generate a unique/new QR code and holds it up.\n")
@@ -109,17 +135,18 @@ func showFHIRExample() {
 			panic(err.Error())
 		}
 
+		proofAsn1string := string(qrEncode(proofAsn1))
+		err = qrcode.WriteFile(proofAsn1string, qrcode.Medium, 512, "qr.png")
+		if err != nil {
+			panic(err.Error())
+		}
+
 		qr := sha256.New()
 		qr.Write(proofAsn1)
 
-		// err1 := ioutil.WriteFile("qr.bin", proofAsn1, 0644)
-		// if err1 != nil {
-		// 	panic(err)
-		// }
+		fmt.Printf("       The QR code contains: %v.... (5.5bit / QR alphanumeric mode encoded)\n", proofAsn1string[:30])
 
-		fmt.Printf("       Sha256 of the QR code is: %x\n", qr.Sum(nil))
-
-		fmt.Printf("       Got proof size of %d bytes (i.e. the size of the Qr code)\n", len(proofAsn1))
+		fmt.Printf("       Got proof size of %d bytes (i.e. the size of the QR code in bytes)\n", len(proofAsn1))
 
 		fmt.Printf("\n")
 
@@ -137,9 +164,9 @@ func showFHIRExample() {
 			fmt.Printf("      so this record was not tamped with.\n")
 
 			rec2 := sha256.New()
-			rec2.Write([]byte(verifiedValues[0]))
+			rec2.Write([]byte(verifiedValues[2]))
 			fmt.Printf("       FHIR level2 Record Hash : %v\n", hex.EncodeToString(rec2.Sum(nil)))
-			fmt.Printf("       FHIR level2 Stored Hash : %v\n", verifiedValues[1])
+			fmt.Printf("       FHIR level2 Stored Hash : %v\n", verifiedValues[3])
 			fmt.Printf("      so this record was not tamped with.\n")
 		}
 	}
